e8055c5a15751f6313c58ad4644e63600a67e26f
Oliver Joseph Ash <oliverjash@gmail.com>
2022-06-25 18:16:18 +0100

    Init

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const ms = require("mockserver-node");
 ms.start_mockserver({
     serverPort: 3030,
+    jvmOptions: [
+        "-Dmockserver.enableCORSForAPI=true",
+        // Error: Could not find or load main class DELETE,
+        // Caused by: java.lang.ClassNotFoundException: DELETE,
+        '-Dmockserver.corsAllowMethods="CONNECT, DELETE, GET, HEAD, OPTIONS, POST, PUT, PATCH, TRACE"',
+    ].join(" "),
 });
